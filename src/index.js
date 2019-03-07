0b8daa6bb941abdb7312cb2f66e796857b5378dc
ihopeyouwin <ihopeyouwon@gmail.com>
2019-03-07 13:23:38 +0300

    feat: added new counter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+ * module.exports = 
+ * function getLoveTrianglesCount(preferences = [3, 3, 3, 3, 3, 3, 3, 3]) {
   var cc = 0;
+  var count = 0;
   for( var i = 0; i < preferences.length; i++)
   {
     if ( i == preferences [ preferences [preferences [i]-1]-1]-1)
     {
       cc++;
+      if( cc == 3)
+      {
+        count++;
+      }
+    }
+    else
+    {
+      continue;
     }
   }
   cc = cc / 3;
-  return cc;
+  return count;
+}
+getLoveTrianglesCount();
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
+  var cc = 0;
+  var count = 0;
+  for( var i = 0; i < preferences.length; i++)
+  {
+    if ( i == preferences [ preferences [preferences [i]-1]-1]-1)
+    {
+      cc++;
+      if( cc == 3)
+      {
+        count++;
+      }
+    }
+    else
+    {
+      continue;
+    }
+  }
+  cc = cc / 3;
+  return count;
 }
